d07da0c0b782f841abae6241bef57eb7c5bd057e
unknown <master.podlaster@gmail.com>
2026-06-01 16:06:09 +0200

    v1 tablica

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World!"<<endl;
- return 0;   
+int t[3][3]={9,0,0,0,0,0,0,0,0};
+for (int i=0;i<3;i++){
+    for (int j=0;j<3;j++)
+    cout<<t[i][j];
+    cout<<endl;
+}
 }
